99280d7aecc52f21260842bd50babd896adce161
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-01-26 16:40:59 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0;
+    
+    sort(array.begin(), array.end());
+    for (int i = 0; i < array.size(); i++) {
+        if (array[i] > height) {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
