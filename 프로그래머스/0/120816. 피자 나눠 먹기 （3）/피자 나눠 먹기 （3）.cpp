9f8c39f63d6090809cac2337ec565907da637b44
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-01-27 04:47:10 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int slice, int n) {
+    int answer = 0;
+    
+    if (n % slice > 0) {
+        answer = n / slice + 1;
+    }
+    else {
+        answer = n / slice;
+    }
+    
+    return answer;
+}
