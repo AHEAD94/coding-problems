b3c7275f7815b6e7e6191ea847c302bf21244fe8
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-02-10 13:36:13 +0900

    [level 0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cpp
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cpp
@@ -5,52 +5,30 @@
 
 using namespace std;
 
+int num;
+
+int compare(int prev, int next) {
+    bool ret;
+    
+    if (abs(prev - num) == abs(next - num)) {
+        ret = next < prev;
+    }
+    else {
+        ret = abs(prev - num) < abs(next - num);
+    }
+    
+    return ret;
+}
+
 vector<int> solution(vector<int> numlist, int n) {
     vector<int> answer;
     vector<int> templist = numlist;
     
-    int front_min_diff = 10001;
-    int rear_min_diff = 10001;
-    int front_diff = 0;
-    int rear_diff = 0;
-    int front = 0;
-    int rear;
+    num = n;
     
-    sort(templist.begin(), templist.end());
+    sort(templist.begin(), templist.end(), compare);
     
-    while (answer.size() < numlist.size()) {
-        rear = templist.size() - 1;
-        
-        front_diff = abs(n - templist[front]);
-        rear_diff = abs(n - templist[rear]);
-        
-        if (front_diff < front_min_diff) {
-            front_min_diff = front_diff;
-        }
-        if (rear_diff < rear_min_diff) {
-            rear_min_diff = rear_diff;
-        }
-        
-        if (front_min_diff > rear_min_diff) {
-            answer.insert(answer.begin(), templist[front]);
-            templist.erase(templist.begin());
-        }
-        else if (front_min_diff < rear_min_diff) {
-            answer.insert(answer.begin(), templist[rear]);
-            templist.erase(templist.end() - 1);
-        }
-        else {
-            if (templist[front] == templist[rear]) {
-                answer.insert(answer.begin(), templist[front]);
-            }
-            else {
-                answer.insert(answer.begin(), min(templist[front], templist[rear]));
-                answer.insert(answer.begin(), max(templist[front], templist[rear]));
-                templist.erase(templist.begin());
-                templist.erase(templist.end() - 1);
-            }
-        }
-    }
+    answer = templist;
     
     return answer;
 }
