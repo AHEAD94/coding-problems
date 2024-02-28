9eb0d3c71bdefdf3fa23a1b055c931f793e2c39c
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-02-28 17:38:51 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -7,12 +8,11 @@ vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     int carpet_size = brown + yellow;
     
-    for (int i = 1; i <= carpet_size; i++) {
+    for (int i = 1; i <= sqrt(carpet_size); i++) {
         if (carpet_size % i == 0) {
             if ((i - 2) * (carpet_size / i - 2) == yellow) {
                 answer.push_back(carpet_size / i);
                 answer.push_back(i);
-                break;
             }
         }
     }
