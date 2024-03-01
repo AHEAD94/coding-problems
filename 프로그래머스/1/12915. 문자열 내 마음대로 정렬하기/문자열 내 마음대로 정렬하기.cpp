ce706c9bff1a9d3419503ee784b380271727ef1b
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-01 22:31:28 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.09 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+typedef struct Pair {
+    char nth_char;
+    string full_string;
+};
+
+bool arbitrary(Pair a, Pair b) {
+    if (a.nth_char < b.nth_char) return true;
+    else if (a.nth_char == b.nth_char) return a.full_string < b.full_string;
+    else return false;
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    vector<string> answer;
+    vector<Pair> pairs;
+    
+    for (int i = 0; i < strings.size(); i ++) {
+        Pair newPair;
+        newPair.nth_char = strings[i][n];
+        newPair.full_string = strings[i];
+        pairs.push_back(newPair);
+    }
+    
+    sort(pairs.begin(), pairs.end(), arbitrary);
+    
+    for (int i = 0; i < pairs.size(); i++) {
+        answer.push_back(pairs[i].full_string);
+    }
+    
+    return answer;
+}
