f3b3045c0ef94552a87668020305ac847f48d754
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-01 22:39:10 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
@@ -6,7 +6,7 @@ using namespace std;
 vector<int> solution(vector<int> num_list) {
     vector<int> answer;
     
-    for (int i = num_list.size()-1; i >= 0; i--) {
+    for (int i = num_list.size() - 1; i >= 0; i--) {
         answer.push_back(num_list[i]);
     }
     
