ebd0d45df3eaefe4ad49a6541abc985fbe3e6321
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-04 23:30:51 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 4.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string message) {
+    int answer = 0;
+    
+    answer = message.length() * 2;
+    
+    return answer;
+}
