d681d52bfb9798ac1e9b2cd7650dcccba5d5a184
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-07 21:10:09 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -3,28 +3,25 @@
 
 using namespace std;
 
+int GCD(int a, int b) {
+    int c;
+    
+    while (b != 0) {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    
+    return a;
+}
+
 vector<int> solution(int n, int m) {
     vector<int> answer;
-    int max_comm_div = 0;
-    int min_comm_multi = 0;
-    int multiple = 0;
+    int gcd = GCD(n, m);
+    int lcm = (n * m) / gcd;
     
-    for (int i = 1; i <= min(n, m); i++) {
-        if (n % i == 0 and m % i == 0) {
-            max_comm_div = i;
-        }
-    }
-    answer.insert(answer.begin(), max_comm_div);
-    
-    multiple = n * m;
-    for (int i = 1; i <= multiple; i++) {
-        if (multiple % i == 0) {
-            if ((multiple / i) % n == 0 and (multiple / i) % m == 0) {
-                min_comm_multi = multiple / i;
-            }
-        }
-    }
-    answer.push_back(min_comm_multi);
+    answer.push_back(gcd);
+    answer.push_back(lcm);
         
     return answer;
 }
