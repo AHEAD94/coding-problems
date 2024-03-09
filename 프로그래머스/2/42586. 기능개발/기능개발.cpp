ef27a2f0bea4364649be87742d1b69347965d74a
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-09 15:38:14 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,27 +1,19 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    queue<int> task_queue;
-    queue<int> speed_queue;
     int cycle = 0;
-    
-    for (int i = 0; i < progresses.size(); i++) {
-        task_queue.push(progresses[i]);
-        speed_queue.push(speeds[i]);
-    }
      
-    while (!task_queue.empty()) {
+    while (!progresses.empty()) {
         int release = 0;
         cycle++;
                 
-        while (!task_queue.empty() and task_queue.front() + (speed_queue.front() * cycle) >= 100) {
-            task_queue.pop();
-            speed_queue.pop();
+        while (!progresses.empty() and progresses.front() + (speeds.front() * cycle) >= 100) {
+            progresses.erase(progresses.begin());
+            speeds.erase(speeds.begin());
             release++;
         }
         
