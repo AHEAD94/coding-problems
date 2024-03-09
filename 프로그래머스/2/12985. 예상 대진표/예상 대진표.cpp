fb5a4192e50264c58d5eaeec17538b74d1d5f775
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-09 12:25:05 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    //cout << "Hello Cpp" << endl;
+
+    while (a != b) {
+        answer++;
+        int remainder_a = a % 2;
+        int remainder_b = b % 2;
+        
+        a /= 2;
+        a += remainder_a;
+        b /= 2;
+        b += remainder_b;
+    }
+    
+    return answer;
+}
