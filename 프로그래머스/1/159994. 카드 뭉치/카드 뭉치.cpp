5779393ab6c7d7bfd9d9c71ddd8c098901249e62
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-11 22:12:30 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -5,24 +5,16 @@ using namespace std;
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
     string answer = "";
+    int idx1 = 0;
+    int idx2 = 0;
     
     for (int i = 0; i < goal.size(); i++) {
-        int deck_number = 0;
-        if (cards1.front() == goal[i]) deck_number = 1;
-        if (cards2.front() == goal[i]) deck_number = 2;
-        
-        switch (deck_number) {
-            case 1:
-                cards1.erase(cards1.begin());
-                break;
-            case 2:
-                cards2.erase(cards2.begin());
-                break;
-            default:
-                answer = "No";
-                break;
+        if (cards1[idx1] == goal[i]) idx1++;
+        else if (cards2[idx2] == goal[i]) idx2++;
+        else {
+            answer = "No";
+            break;
         }
-        if (answer == "No") break;
     }
     
     if (answer != "No") answer = "Yes";
