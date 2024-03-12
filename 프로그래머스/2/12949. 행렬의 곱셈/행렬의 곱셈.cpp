13399c8509bd4cb29677634fb73b1d0eda654c44
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-12 22:16:05 +0900

    [level 2] Title: 행렬의 곱셈, Time: 0.99 ms, Memory: 4.55 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -7,23 +7,21 @@ vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
     vector<vector<int>> answer;
     
     for (int arr1_row = 0; arr1_row < arr1.size(); arr1_row++) {
-        vector<int> new_row;
-        int arr1_columns = arr1[0].size();
-        int arr2_columns = arr2[0].size();
+        vector<int> answer_row;
         int j = 0;
         
-        for (int arr2_column = 0; arr2_column < arr2_columns; arr2_column++) {
+        while (j < arr2[0].size()) {
             int i = 0;
             int temp_calc = 0;
             
-            while (i < arr1_columns) {
+            while (i < arr1[0].size()) {
                 temp_calc += arr1[arr1_row][i] * arr2[i][j];
                 i++;
             }
-            new_row.push_back(temp_calc);
+            answer_row.push_back(temp_calc);
             j++;
         }
-        answer.push_back(new_row);
+        answer.push_back(answer_row);
     }
     
     return answer;
