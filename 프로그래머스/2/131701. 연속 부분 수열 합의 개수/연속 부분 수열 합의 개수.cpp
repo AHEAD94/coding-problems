2fb26b55d68997f573bd063182843b7f1f26ba44
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-21 22:07:12 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 520.50 ms, Memory: 22.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -9,13 +9,24 @@ int solution(vector<int> elements) {
     int tot_size = elements.size();
     set<int> num_set;
     
-    for (int sub_size = 1; sub_size <= tot_size; sub_size++) { // 부분문자열 길이 증가
-        for (int i = 0; i < tot_size; i++) { // 전체 문자열 순회
-            int sum = 0;
+//     for (int sub_size = 1; sub_size <= tot_size; sub_size++) { // 부분문자열 길이 증가
+//         for (int i = 0; i < tot_size; i++) { // 전체 문자열 순회
+//             int sum = 0;
             
-            for (int j = i; j < i + sub_size; j++) { // 부분문자열 길이만큼 순회
-                sum += elements[j % tot_size];
-            }
+//             for (int j = i; j < i + sub_size; j++) { // 부분문자열 길이만큼 순회
+//                 sum += elements[j % tot_size];
+//             }
+//             num_set.insert(sum);
+//         }
+//     }
+//     answer = num_set.size();
+    
+    for (int i = 0; i < tot_size; i++) { // 전체 문자열 순회
+        int sum = 0;
+
+        // 시작 인덱스로부터 모든 부분문자열 길이만큼 합 계산
+        for (int j = i; j < i + tot_size; j++) { 
+            sum += elements[j % tot_size];
             num_set.insert(sum);
         }
     }
