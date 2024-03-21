5a4900b77aa6f87539512551f100decd3e18469b
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-21 21:51:50 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1274.98 ms, Memory: 23.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int solution(vector<int> elements) {
+    int answer = 0;
+    int tot_size = elements.size();
+    set<int> num_set;
+    
+    for (int sub_size = 1; sub_size <= tot_size; sub_size++) { // 부분문자열 길이 증가
+        for (int i = 0; i < tot_size; i++) { // 전체 문자열 순회
+            int sum = 0;
+            
+            for (int j = i; j < i + sub_size; j++) { // 부분문자열 길이만큼 순회
+                sum += elements[j % tot_size];
+            }
+            num_set.insert(sum);
+        }
+    }
+    answer = num_set.size();
+    
+    return answer;
+}
