89e4ca6fca0df37bce187c9db929172a76e66778
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-25 22:05:00 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> overlap(vector<int> line1, vector<int> line2) {    
+    if (line1[0] <= line2[0]) {
+        if (line1[1] <= line2[0]) return {};
+        if (line2[1] <= line1[1]) return line2;
+        if (line1[1] <= line2[1]) return {line2[0], line1[1]};
+    }
+    else {
+        if (line2[1] <= line1[0]) return {};
+        if (line1[1] <= line2[1]) return line1;
+        if (line2[1] <= line1[1]) return {line1[0], line2[1]};
+    }
+    return {};
+}
+
+int solution(vector<vector<int>> lines) {
+    int answer = 0;
+    vector<vector<int>> overlapped;
+    
+    for (int i = 0; i < lines.size() - 1; i++) {
+        for (int j = i + 1; j < lines.size(); j++) {
+            vector<int> temp_line;
+            temp_line = overlap(lines[i], lines[j]);
+            
+            if (!temp_line.empty()) {
+                overlapped.push_back(temp_line);
+                answer += temp_line[1] - temp_line[0];
+            }
+        }
+    }
+    
+    if (overlapped.size() >= 2) {
+        for (int i = 0; i < overlapped.size() - 1; i++) {
+            vector<int> temp_line;
+            temp_line = overlap(overlapped[i], overlapped[i + 1]);
+
+            if (!temp_line.empty()) 
+                answer -= temp_line[1] - temp_line[0];
+        }
+    }
+        
+    return answer;
+}
