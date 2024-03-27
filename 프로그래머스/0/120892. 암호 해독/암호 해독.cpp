6d0afb69e27ccbabb9a111715ebcb9bd063cc18e
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-27 21:43:23 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.cpp
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.cpp
@@ -7,9 +7,8 @@ string solution(string cipher, int code) {
     string answer = "";
     
     for (int i = 0; i < cipher.length(); i++) {
-        if ((i + 1) % code == 0) {
+        if ((i + 1) % code == 0)
             answer += cipher[i];
-        }
     }
     
     return answer;
