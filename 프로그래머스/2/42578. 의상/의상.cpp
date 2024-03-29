ff601a47b3dbdb4ad53b73764889e605a6dc1b70
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-03-29 19:46:33 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map<string, int> clo_num;
+    
+    for (int i = 0; i < clothes.size(); i++) {
+        clo_num[clothes[i][1]]++;
+    }
+    
+    for (pair<string, int> clo : clo_num) {
+        answer *= clo.second + 1; // 입지 않는 경우 포함
+    }
+    answer--; // 아무것도 입지 않는 경우 제외
+    
+    return answer;
+}
