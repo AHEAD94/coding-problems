8f7f64ef71ceee068caf8eddd250e93b92ccd6d5
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-04-01 19:58:47 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.02 ms
+메모리: 4.17 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 31일 14:41:30
+2024년 04월 01일 19:58:45
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.cpp
+++ 프로그래머스/2/64065. 튜플/튜플.cpp
@@ -1,55 +1,40 @@
 #include <string>
 #include <vector>
-#include <stack>
+#include <map>
 #include <algorithm>
-#include <set>
 
 using namespace std;
 
-bool bySize(vector<int> a, vector<int> b) {
-    return a.size() < b.size();
+bool byValue(pair<int, int> a, pair<int, int> b) {
+    if (a.second == b.second) return a.first > b.first;
+    return a.second > b.second;
 }
 
 vector<int> solution(string s) {
     vector<int> answer;
-    stack<char> bracket;
-    vector<vector<int>> tuples;
-    vector<int> tuple;
-    string temp_st = "";
-    set<int> num_set;
+    map<int, int> num_cnt_map;
+    string st_token = "";
     
-    for (int i = 1; i < s.length() - 1; i++) { // 양 끝 중괄호 무시        
-        int element = 0;
-        
-        // int형으로 바꾸어 tuple에 원소 저장
-        if (!bracket.empty()) {
-            if (s[i] == ',' or s[i] == '}') {
-                element = stoi(temp_st);
-                tuple.push_back(element);
-                temp_st = "";
-            }
-            else temp_st += s[i];
+    for (int i = 1; i < s.length() - 1; i++) { // 양 끝 중괄호 무시                
+        if ('0' <= s[i] and s[i] <= '9') {
+            st_token += s[i];
         }
         
-        if (s[i] == '{') bracket.push(s[i]);
-        if (s[i] == '}') {
-            tuples.push_back(tuple); // tuple을 tuples에 저장
-            bracket.pop();
-            tuple = vector<int>();
-            temp_st = "";
+        // map에 숫자와 숫자의 카운트를 저장
+        if (s[i] == ',' or s[i] == '}') {
+            if (st_token != "") {
+                num_cnt_map[stoi(st_token)]++;
+                st_token = "";
+            }
         }
     }
     
-    sort(tuples.begin(), tuples.end(), bySize);
+    vector<pair<int, int>> sorting_vect(num_cnt_map.begin(), num_cnt_map.end());
+    sort(sorting_vect.begin(), sorting_vect.end(), byValue);
     
-    for (vector<int> tuple : tuples) {
-        for (int element : tuple) {
-            set<int>::iterator iter = num_set.find(element);
-            if (iter == num_set.end()) {
-                num_set.insert(element);
-                answer.push_back(element);
-            }
-        }
+    // 많이 나타난 숫자의 카운트 순으로 answer에 삽입
+    for (pair<int, int> num_cnt_pair : sorting_vect) {
+        answer.push_back(num_cnt_pair.first);
     }
     
     return answer;
