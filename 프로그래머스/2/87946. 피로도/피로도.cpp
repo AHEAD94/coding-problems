771348d1db3f5940329b24d66218cf403e48b336
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-04-07 13:00:44 +0900

    [level 2] Title: 피로도, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -3,24 +3,20 @@
 
 using namespace std;
 
-void DFS(int k, vector<vector<int>> &dungeons, vector<bool> &visited, int count, int &answer) {
+void DFS(int k, vector<vector<int>> &dungeons, vector<bool> &visited, int depth, int &answer) {
     for (int i = 0; i < dungeons.size(); i++) {
         if (k >= dungeons[i][0] and visited[i] == false) {
             visited[i] = true;
-            DFS(k - dungeons[i][1], dungeons, visited, count + 1, answer);
-            
-            // 방문 해제
+            DFS(k - dungeons[i][1], dungeons, visited, depth + 1, answer);
             visited[i] = false;
         }
     }
     
-    if (count > answer) answer = count;
-    if (answer == dungeons.size()) return;
+    if (depth > answer) answer = depth;
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-    int answer = 0;
-    int count = 0;
+    int answer = -1;
     vector<bool> visited(dungeons.size(), false);
     
     DFS(k, dungeons, visited, 0, answer);
