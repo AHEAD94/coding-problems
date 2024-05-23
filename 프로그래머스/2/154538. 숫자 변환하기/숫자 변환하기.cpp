912916a153affecb840c60bc1b99e2b5dd23cac0
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-05-23 21:47:10 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int BFS(int x, int y, int n) {
+    int calc_num = 0;
+    bool is_answer_found = false;
+    
+    queue<pair<int, int>> num_q;
+    num_q.push(make_pair(y, 0));
+    
+    while (!num_q.empty()) {
+        int num = num_q.front().first;
+        calc_num = num_q.front().second;
+        num_q.pop();
+                
+        if (num == x) {
+            is_answer_found = true;
+            break;
+        }
+        
+        if (num % 3 == 0 and num / 3 >= x)
+            num_q.push(make_pair(num / 3, calc_num + 1));
+        if (num % 2 == 0 and num / 2 >= x)
+            num_q.push(make_pair(num / 2, calc_num + 1));
+        if (num - n >= x)
+            num_q.push(make_pair(num - n, calc_num + 1));
+    }
+    
+    if (is_answer_found == false) calc_num = -1;
+    
+    return calc_num;
+}
+
+int solution(int x, int y, int n) {
+    int answer = 0;
+    
+    answer = BFS(x, y ,n);
+    
+    return answer;
+}
