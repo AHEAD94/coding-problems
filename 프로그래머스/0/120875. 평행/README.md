767cfc22b8e0f6634ad253c307421928438ea214
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-06-02 13:28:48 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/README.md
+++ 프로그래머스/0/120875. 평행/README.md
@@ -1,10 +1,10 @@
 # [level 0] 평행 - 120875 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.01 ms
+메모리: 4.21 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 01일 21:18:12
+2024년 06월 02일 13:28:43
 
 ### 문제 설명
 
--- 프로그래머스/0/120875. 평행/평행.cpp
+++ 프로그래머스/0/120875. 평행/평행.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <tuple>
 
 using namespace std;
 
@@ -12,28 +11,49 @@ double GetSlope(vector<int> dot1, vector<int> dot2) {
     return slope;
 }
 
-int solution(vector<vector<int>> dots) {
-    int answer = 0;
-    vector<tuple<vector<int>, vector<int>, double>> lines;
-
+void DFS(vector<vector<int>> dots, vector<bool> &visited, vector<pair<vector<int>, vector<int>>> &lines) {    
     for (int i = 0; i < 3; i++) {
         for (int j = i + 1; j < 4; j++) {
-            double slope = GetSlope(dots[i], dots[j]);
-            lines.push_back({dots[i], dots[j], slope});
-        }
-    }
-        
-    int lines_size = lines.size();
-    for (int i = 0; i < lines_size - 1; i++) {
-        for (int j = i + 1; j < lines_size; j++) {
-            if (get<2>(lines[i]) == get<2>(lines[j]) and
-                get<0>(lines[i]) != get<0>(lines[j]) and
-                get<0>(lines[i]) != get<1>(lines[j]) and
-                get<1>(lines[i]) != get<0>(lines[j]) and
-                get<1>(lines[i]) != get<1>(lines[j])) 
-                answer = 1;
+            if (!visited[i] and !visited[j]) {
+                visited[i] = true;
+                visited[j] = true;
+                
+                lines.push_back({dots[i], dots[j]});
+                DFS(dots, visited, lines);
+                
+                visited[i] = false;
+                visited[j] = false;
+            }
         }
     }
     
+    return;
+}
+
+int solution(vector<vector<int>> dots) {
+    int answer = 0;
+    vector<bool> visited(4, false);
+    vector<pair<vector<int>, vector<int>>> lines;
+
+    DFS(dots, visited, lines);
+    
+    pair<vector<int>, vector<int>> line1;
+    pair<vector<int>, vector<int>> line2;
+    
+    int lines_size = lines.size();
+    for (int i = 0; i < lines_size; i++) {
+        if (i % 2 == 0) line1 = lines[i];
+        else {
+            line2 = lines[i];
+            
+            double slope1 = GetSlope(line1.first, line1.second);
+            double slope2 = GetSlope(line2.first, line2.second);
+            if (slope1 == slope2) {
+                answer = 1;
+                break;
+            } 
+        }
+    }
+        
     return answer;
 }
