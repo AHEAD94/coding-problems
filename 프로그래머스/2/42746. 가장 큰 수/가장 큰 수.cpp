5c6cf48ed2d2732c24de1171ed3e2598c2b81dd1
AHEAD94 <43318103+AHEAD94@users.noreply.github.com>
2024-06-15 21:16:31 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+bool SortByDigit(string a, string b) {
+    return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<string> str_nums;
+    
+    for (int num : numbers) {
+        str_nums.push_back(to_string(num));
+    }
+    
+    sort(str_nums.begin(), str_nums.end(), SortByDigit);
+    
+    for (string str_num : str_nums) {
+        answer += str_num;
+    }
+    
+    if (answer.front() == '0') answer = "0";
+    
+    return answer;
+}
