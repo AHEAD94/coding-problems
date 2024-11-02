3dc0243626592d45b167ccd1b99b8a673a50a88d
DAEHA YOO <ulysses94@naver.com>
2024-11-02 18:21:47 +0900

    [Silver V] Title: 단어 정렬, Time: 20 ms, Memory: 4932 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -3,34 +3,30 @@
 
 using namespace std;
 
-// set 의 정렬 기준을 바꾸기 위한 구조체 Compare 정의
-struct Compare {
-	bool operator() (const string &a, const string &b) const{
-		if (a.size() == b.size())
-			return a < b;
-		else
-			return a.size() < b.size();
-	}
+struct CompByLen {
+    bool operator()(const string& a, const string& b) const {
+        if (a.size() == b.size()) return a < b;
+        return a.size() < b.size();
+    }
 };
 
 int main() {
-
-	ios::sync_with_stdio(0);
+    ios::sync_with_stdio(0);
 	cin.tie(0);
     
-	int N;
-	set<string, Compare> sets; // 이와 같이 선언
-	cin >> N;
-	string temp;
-
-	for (int i = 0; i < N; i++) {
-		cin >> temp;
-		sets.insert(temp);
-	}
-
-	for (auto str : sets) {
-		cout << str << '\n';
-	}
-
-	return 0;
+    int N;
+    cin >> N;
+    
+    set<string, CompByLen> str_set;
+    string temp;
+    for (int i = 0; i < N; i++) {
+        cin >> temp;
+        str_set.insert(temp);
+    }
+    
+    for (auto str : str_set) {
+        cout << str << "\n";
+    }
+    
+    return 0;
 }
