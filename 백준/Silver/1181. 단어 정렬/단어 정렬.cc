2dd31a2e9e02949b78f1befd9e66e4d236fd2af4
DAEHA YOO <ulysses94@naver.com>
2024-11-02 18:10:01 +0900

    [Silver V] Title: 단어 정렬, Time: 20 ms, Memory: 4932 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,30 +1,36 @@
 #include <iostream>
-#include <string>
 #include <set>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-bool ByLen(const string a, const string b) {
-    if (a.size() == b.size()) return a < b;
-    else return a.size() < b.size();
-}
+// set 의 정렬 기준을 바꾸기 위한 구조체 Compare 정의
+struct Compare {
+	bool operator() (const string &a, const string &b) const{
+		if (a.size() == b.size())
+			return a < b;
+		else
+			return a.size() < b.size();
+	}
+};
 
 int main() {
-    int N;
-    cin >> N;
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
     
-    set<string> str_set;
-    for (int i = 0; i < N; i++) {
-        string temp;
-        cin >> temp;
-        str_set.insert(temp);
-    }
-    
-    vector<string> str_vec(str_set.size(), "");
-    copy(str_set.begin(), str_set.end(), str_vec.begin());
-    sort(str_vec.begin(), str_vec.end(), ByLen);
-    
-    for (auto str : str_vec) cout << str << endl;
+	int N;
+	set<string, Compare> sets; // 이와 같이 선언
+	cin >> N;
+	string temp;
+
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		sets.insert(temp);
+	}
+
+	for (auto str : sets) {
+		cout << str << '\n';
+	}
+
+	return 0;
 }
