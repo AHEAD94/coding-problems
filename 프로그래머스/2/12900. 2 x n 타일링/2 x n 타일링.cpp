9ddd6f9121173368ab255ada6cedd5389ff4d08d
DAEHA YOO <ulysses94@naver.com>
2025-01-22 22:25:33 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.42 ms, Memory: 3.95 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -5,14 +5,18 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    vector<int> fibo;
-    // 0 // 1 // 2 // 3 // 5 // 8 // ...
-    for (int i = 0; i <= n; i++) {
-        if (i < 3) fibo.push_back(i);
-        else fibo.push_back(fibo[i - 2] % 1000000007 + fibo[i - 1] % 1000000007);
-    }
     
-    answer = fibo[fibo.size() - 1] % 1000000007;
+    vector<int> cases;
+    cases.push_back(0);
+    cases.push_back(1);
+    cases.push_back(2);
+
+    while (cases.size() <= n) {
+        int size = cases.size();
+        int dp_val = cases[size - 2] + cases[size - 1];
+        cases.push_back(dp_val % 1000000007);
+    }
+    answer = cases[n];
     
     return answer;
 }
